eb5f0a8581251f6a6b9d40f6836a79aa392e06e2
sanjay-personal <sanjay.gajjala1995@gmail.com>
2022-09-22 08:48:47 +0530

    layout ready

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
